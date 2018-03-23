305bef9b9769fd4fd805ebc75b801558c6f822c6
alyssamhope <berkeleycole@users.noreply.github.com>
2018-03-23 14:26:30 -0700

    Create Week-4.rb

---
+++ Week-4.rb
@@ -0,0 +1,30 @@
+#### 1. Use Ruby to loop over this array, multiplying each element by 2.
+
+tempArray = [1, 2, 6, 9, 3, 21]
+
+# your ruby loop here
+
+#### 2. From all the built in Ruby methods we've seen in class this week, choose three that you think are particularly helpful and create examples to show how they work.
+
+# your three built in ruby methods
+
+#### 3. Look at this horrible ruby code, and fix it to be good ruby code. 
+
+``` ruby
+class example
+  def initialize(day)
+    @day=day
+  end
+  
+  def Say_hi
+    if(day == "Friday"){
+      puts "TGIF!"
+    }
+    elseif(day == "Monday"){
+      puts "Its monday again"
+    }
+    else
+      puts "another day"
+  end
+end
+```
